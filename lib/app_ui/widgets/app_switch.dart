ef25ac85b0d3693e3fec7f5bd8ad00b05795e87a
Gevorg <31342302+GevTonoyan@users.noreply.github.com>
2025-12-28 23:16:20 +0400

    Change app switch track border color

---
+++ lib/app_ui/widgets/app_switch.dart
@@ -0,0 +1,27 @@
+import 'package:boardify/utils/extensions/context_extension.dart';
+import 'package:flutter/material.dart';
+
+class AppSwitch extends StatelessWidget {
+  const AppSwitch({required this.value, required this.onChanged, super.key});
+
+  final bool value;
+  final ValueChanged<bool>? onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = context.colors;
+
+    return Switch(
+      activeThumbColor: colors.green,
+      activeTrackColor: colors.white,
+      inactiveThumbColor: colors.white,
+      inactiveTrackColor: colors.white20,
+      trackOutlineColor: WidgetStateProperty.resolveWith(
+        (_) => colors.white20,
+      ),
+      padding: EdgeInsets.zero,
+      value: value,
+      onChanged: onChanged,
+    );
+  }
+}
