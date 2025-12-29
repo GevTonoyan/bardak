0e9c9732301404f324d94dbb97acaedb7238b817
Gevorg <31342302+GevTonoyan@users.noreply.github.com>
2025-12-29 13:33:34 +0400

    Add reusable screen background widget

---
+++ lib/app_ui/widgets/screen_background.dart
@@ -0,0 +1,35 @@
+import 'package:boardify/assets/assets.gen.dart';
+import 'package:boardify/utils/extensions/context_extension.dart';
+import 'package:flutter/material.dart';
+
+class ScreenBackground extends StatelessWidget {
+  const ScreenBackground({
+    required this.child,
+    this.shadowHeight,
+    super.key,
+  });
+
+  final Widget child;
+  final double? shadowHeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: DecoratedBox(
+              decoration: BoxDecoration(gradient: context.colors.main),
+            ),
+          ),
+          if (shadowHeight != null)
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Assets.shadowEffect.svg(height: shadowHeight),
+            ),
+          child,
+        ],
+      ),
+    );
+  }
+}
