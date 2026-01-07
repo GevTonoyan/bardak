e83ac3aafa0f572dc37cb299ec87589fe307d7bf
Gevorg <31342302+GevTonoyan@users.noreply.github.com>
2026-01-08 00:08:18 +0400

    Add show confirm sheet widget

---
+++ lib/app_ui/widgets/show_confirm_sheet.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+
+import 'package:boardify/app_ui/widgets/app_button.dart';
+import 'package:boardify/app_ui/widgets/app_icon_button.dart';
+import 'package:boardify/app_ui/widgets/app_spacings.dart';
+import 'package:boardify/utils/extensions/context_extension.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+Future<void> showConfirmSheet({
+  required BuildContext context,
+  required String title,
+  required String description,
+  required String confirmText,
+  required String cancelText,
+  required VoidCallback onConfirm,
+  required VoidCallback onCancel,
+}) async {
+  await showModalBottomSheet<void>(
+    context: context,
+    backgroundColor: context.colors.secondary,
+    builder: (BuildContext context) {
+      final colors = context.colors;
+      final textStyle = context.typography.regular24.copyWith(
+        color: colors.white,
+      );
+
+      return Material(
+        color: colors.secondary,
+        borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisSize: .min,
+              children: [
+                SizedBox(
+                  height: 40,
+                  child: Stack(
+                    children: [
+                      Center(
+                        child: Text(title, style: textStyle),
+                      ),
+                      AppIconButton.back(onTap: () => context.pop()),
+                    ],
+                  ),
+                ),
+                height40,
+                Text(description, textAlign: .center, style: textStyle),
+                height40,
+                AppButton(
+                  color: colors.red,
+                  label: cancelText,
+                  onPressed: onCancel,
+                ),
+                height20,
+                AppButton(
+                  color: colors.green,
+                  label: confirmText,
+                  onPressed: onConfirm,
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    },
+  );
+}
