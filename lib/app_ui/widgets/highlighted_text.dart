bec975269bb68df051a538c100b83d258b6d7c6c
Gevorg <31342302+GevTonoyan@users.noreply.github.com>
2026-01-09 19:57:44 +0400

    Introduce highlighted text widget

---
+++ lib/app_ui/widgets/highlighted_text.dart
@@ -0,0 +1,28 @@
+import 'package:boardify/utils/extensions/context_extension.dart';
+import 'package:flutter/material.dart';
+
+class HighlightedText extends StatelessWidget {
+  const HighlightedText({required this.text, super.key});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = context.colors;
+    final typography = context.typography;
+
+    return Container(
+      padding: const .all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: colors.secondary,
+      ),
+      child: Text(
+        text,
+        style: typography.regular28.copyWith(
+          color: colors.white,
+        ),
+      ),
+    );
+  }
+}
