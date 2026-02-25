2bb39cbfaa056b2f4c6c234e1ee58692f91f25b2
Gevorg <31342302+GevTonoyan@users.noreply.github.com>
2026-02-25 13:25:48 +0400

    cleanup some dead code

--- lib/pre_game/presentation/bloc/pre_game_bloc.dart
+++ lib/pre_game/presentation/bloc/pre_game_bloc.dart
@@ -1,6 +1,4 @@
 import 'package:boardify/pre_game/domain/entities/pre_game_entity.dart';
-import 'package:boardify/word_pack/domain/usecases/get_word_packs_usecase.dart';
-import 'package:boardify/word_pack/domain/usecases/get_words_by_pack_usecase.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -9,14 +7,10 @@ part 'pre_game_event.dart';
 part 'pre_game_state.dart';
 
 class PreGameBloc extends Bloc<PreGameEvent, PreGameState> {
-  PreGameBloc({required this.getWordPacks, required this.getWordsByPack})
-    : super(PreGameState.initial()) {
+  PreGameBloc() : super(PreGameState.initial()) {
     on<ChangeGameModeEvent>(_changeGameMode);
     on<AddTeamsEvent>(_addTeams);
   }
-
-  final GetWordPacksUseCase getWordPacks;
-  final GetWordsByPackUseCase getWordsByPack;
 
   void _changeGameMode(ChangeGameModeEvent event, Emitter<PreGameState> emit) {
     if (state.gameMode == event.gameMode) return;
