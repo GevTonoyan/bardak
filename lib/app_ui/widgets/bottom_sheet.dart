b0c350b6982e284350dc1a45d4ad83504ccb886f
Gevorg <31342302+GevTonoyan@users.noreply.github.com>
2026-03-01 13:18:53 +0400

    Make bottom sheets barrier color as blurred

--- lib/app_ui/widgets/bottom_sheet.dart
+++ lib/app_ui/widgets/bottom_sheet.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:alias_pro/app_ui/widgets/app_icon_button.dart';
 import 'package:alias_pro/utils/extensions/context_extension.dart';
 import 'package:flutter/material.dart';
@@ -17,36 +19,46 @@ Route<T> buildAppBottomSheetRoute<T>({
       final colors = context.appTheme.colors;
       final typography = context.appTheme.typography;
 
-      return Material(
-        color: colors.secondary,
-        borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
-        child: SafeArea(
-          child: Padding(
-            padding: const .all(20),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SizedBox(
-                  height: 40,
-                  child: Stack(
-                    children: [
-                      Center(
-                        child: Text(
-                          title,
-                          style: typography.regular24.copyWith(
-                            color: colors.white,
-                          ),
-                        ),
-                      ),
-                      AppIconButton.back(onTap: () => context.pop()),
-                    ],
-                  ),
-                ),
-                child,
-              ],
+      return Stack(
+        children: [
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: const SizedBox.expand(),
             ),
           ),
-        ),
+          Material(
+            color: colors.secondary,
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
+            child: SafeArea(
+              child: Padding(
+                padding: const .all(20),
+                child: Column(
+                  mainAxisSize: .min,
+                  children: [
+                    SizedBox(
+                      height: 40,
+                      child: Stack(
+                        children: [
+                          Center(
+                            child: Text(
+                              title,
+                              style: typography.regular24.copyWith(
+                                color: colors.white,
+                              ),
+                            ),
+                          ),
+                          AppIconButton.back(onTap: () => context.pop()),
+                        ],
+                      ),
+                    ),
+                    child,
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       );
     },
   );
