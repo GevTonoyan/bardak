a466d265a0d6c6bee3b53a23f12465ab537f4cc1
Gevorg <31342302+GevTonoyan@users.noreply.github.com>
2026-04-02 15:41:10 +0400

    Add text with border widget

---
+++ lib/app_ui/widgets/text_with_border.dart
@@ -0,0 +1,48 @@
+import 'package:alias_pro/utils/extensions/context_extension.dart';
+import 'package:flutter/material.dart';
+
+class TextWithBorder extends StatelessWidget {
+  const TextWithBorder(
+    this.text, {
+    required this.style,
+    this.borderWidth = 5.0,
+    this.borderColor,
+    this.textColor,
+    super.key,
+  });
+
+  final String text;
+  final TextStyle style;
+  final double borderWidth;
+  final Color? borderColor;
+  final Color? textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = context.colors;
+
+    return Stack(
+      children: [
+        // 1. THE BORDER (Background layer)
+        Text(
+          text,
+          style: style.copyWith(
+            foreground: Paint()
+              ..style = .stroke
+              ..strokeWidth = borderWidth
+              ..strokeCap = StrokeCap.round
+              ..strokeJoin = StrokeJoin.round
+              ..color = borderColor ?? colors.black,
+          ),
+        ),
+        // 2. THE FILL (Foreground layer)
+        Text(
+          text,
+          style: style.copyWith(
+            color: textColor ?? colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+}
